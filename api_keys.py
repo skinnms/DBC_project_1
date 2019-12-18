93750edeb230b18e7407778148ae37e951f1e12e
loupacmatt <loupacmatt@gmail.com>
2019-12-17 19:45:09 -0600

    adding gitignore for api keys

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,1 @@
-# available data sets and documenations located @ https://apps.bea.gov/api/signup/
-bea_api_key = "F22427A1-A5CE-4B41-8E6B-22468F9781D9"
-
-# available data sets and documentation located @ https://api.census.gov/data.html
-cb_api_key = "2b859a84c5eb4216cee374e57981bd715efa859c"
+api_key = "#"
